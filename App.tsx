8309c9d0822e649213974ceeb75fd7123ba9cc3d
Faisal <m.faisal.s@outlook.com>
2022-09-16 17:48:52 +0530

    ad

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, View, Switch } from 'react-native';
+import { StyleSheet, View, Switch, SafeAreaView } from 'react-native';
 import { ThemeContext } from './context/ThemeContext'; // Allows global declaration of theme
 import { myColors } from './styles/Colors';
 import MyKeyboard from './components/MyKeyboard';
@@ -8,13 +8,13 @@ export default function App() {
   const [theme, setTheme] = useState('light');
   return (
     <ThemeContext.Provider value={theme}> 
-      <View style={theme === 'light' ? styles.containerlight : styles.containerdark}>
+      <SafeAreaView style={theme === 'light' ? styles.containerlight : styles.containerdark}>
       <Switch
         value={theme === 'dark'} // Switch on / true for whichever value is passed
         onValueChange={() => setTheme(theme === 'light'? 'dark' : 'light')}
       />
       <MyKeyboard/>
-    </View>
+    </SafeAreaView>
     </ThemeContext.Provider>
   );
 }
@@ -24,12 +24,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start'
   },
   containerdark: {
     flex: 1,
     backgroundColor: myColors.dark,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'flex-start'
   },
 });
--- components/MyKeyboard.tsx
+++ components/MyKeyboard.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 import Button from './Button'
 import { Styles } from '../styles/GlobalStyles'
 import { myColors } from '../styles/Colors'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 const myKeyboard = () => {
 
@@ -55,12 +56,62 @@ const myKeyboard = () => {
     }
   };
 
+  const firstNumberDisplay = () => {
+    if(result !== null){
+      return <Text style={result<99999 ? [Styles.screenFirstNumber, {color: myColors.result}] : [Styles.screenFirstNumber, {fontSize: 50,color: myColors.result}]}>{result?.toString()}</Text>;
+    }
+
+    if( firstNumber && firstNumber.length < 6){
+      return <Text style={Styles.screenFirstNumber}></Text>;
+    }
+
+    if (firstNumber === "") {
+      return <Text style={Styles.screenFirstNumber}>{"0"}</Text>;
+    }
+    
+    if (firstNumber.length > 5 && firstNumber.length < 8) {
+      return (
+        <Text style={[Styles.screenFirstNumber, { fontSize: 70 }]}>
+          {firstNumber}
+        </Text>
+      );
+    }
+
+    if (firstNumber. length > 7) {
+      return (
+        <Text style={[Styles.screenFirstNumber, { fontSize: 50 }]}>
+        {firstNumber}
+        </Text>
+      );
+    }
+  };
+
   // <></> : Fragments
   // A common pattern in React is for a component to return multiple elements
   // Fragments let you group a list of children without adding extra nodes to the DOM
 
   return (
     <View style={Styles.viewBottom}>
+      <View
+        style={{
+          height: 120,
+          width: '90%',
+          justifyContent: 'flex-end',
+          alignSelf: 'center',
+        }}
+      >
+        <Text style={Styles.screenSecondNumber}>
+          {secondNumber}
+          <Text style={{
+            color: 'purple',
+            fontSize: 50,
+            fontWeight: '500',
+          }}>
+            {operation}
+          </Text>
+        </Text>
+        {firstNumberDisplay()}
+      </View>
       <View style={Styles.row}>
         <Button title='C' isGray onPress={clear} />
         <Button title='+/-' isGray onPress={() => handleOperationPress("+/-")} />
@@ -84,6 +135,12 @@ const myKeyboard = () => {
         <Button title='2' onPress={() => handleNumberPress('2')} />
         <Button title='3' onPress={() => handleNumberPress('3')} />
         <Button title='+' isBlue onPress={() => handleOperationPress("+")} />
+      </View>
+      <View style={Styles.row}>
+        <Button title='.' onPress={() => handleNumberPress('.')} />
+        <Button title='0' onPress={() => handleNumberPress('0')} />
+        <Button title='DEL' onPress={() => setFirstNumber(firstNumber.slice(0,-1))} />
+        <Button title='=' isBlue onPress={() => getResult()} />
       </View>
     </View>
   )
